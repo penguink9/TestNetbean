8f7ae5aa6a5bf55bb0c64c7439cdf2c1f456876a
penguink9 <giangqt2k4@gmail.com>
2024-09-21 22:39:34 +0700

    Add DBContext

---
+++ src/main/java/dbcontext/DBContext.java
@@ -0,0 +1,26 @@
+
+package dbcontext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try  {
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://DESKTOP-Q9874N2;databaseName=SneakerShop;encrypt=false;trustServerCertificate=false;loginTimeout=30;";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }   
+    }
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+    }
+}
